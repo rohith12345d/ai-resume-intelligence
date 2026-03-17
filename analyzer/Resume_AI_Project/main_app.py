7de6a47bff891621cfb34a84f4bdddbf084de212
rohith12345d <rohith12345d@gmail.com>
2026-03-17 13:55:34 +0530

    Update main_app.py

--- analyzer/Resume_AI_Project/main_app.py
+++ analyzer/Resume_AI_Project/main_app.py
@@ -143,7 +143,9 @@ skill_values = list(skills.values())
 # 📊 SKILL ANALYSIS PAGE
 # =====================================================
 if menu == "📊 Skill Analysis":
-
+    with st.sppinner('AI is analyzing skill data..."):
+                     time.sleep(1)
+    st.subheader("Detected Skills")
     # ---------- AI RESUME STRENGTH ----------
     score = calculate_readiness(skill_names)
 
@@ -274,6 +276,8 @@ if menu == "📊 Skill Analysis":
 # 🎯 CAREER MATCH PAGE
 # =====================================================
 elif menu == "🎯 Career Match":
+    with st.sppinner('AI is analyzing career compatibility..."):
+                     time.sleep(1)
 
     st.subheader("Career Recommendations")
 
@@ -325,6 +329,8 @@ elif menu == "🎯 Career Match":
 # 🧠 SKILL GAP ROADMAP
 # =====================================================
 elif menu == "🧠 Skill Gap Roadmap":
+    with st.sppinner('AI is generating learning roadmap..."):
+                     time.sleep(1)
 
     st.subheader("Learning Roadmap")
 
@@ -341,6 +347,8 @@ elif menu == "🧠 Skill Gap Roadmap":
 
 # =====================================================
 elif menu == "💡 AI Insights":
+    with st.sppinner('AI is generating resume insights..."):
+                     time.sleep(1)
 
     st.subheader("AI Resume Insights")
 
